2755ca082d01834873bd561c179d127a3bb9472c
Fabricio Silva <fabriciosilvam98@gmail.com>
2023-03-31 14:53:35 -0300

    Criando Domain com Cliente, adicionando referencias ao entityframework e mapeando Domain como DbSet

---
+++ AR.Domain/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AR.Domain
+{
+    //Classe de nome Cliente
+    public class Cliente
+    {
+        [Key]
+        //Propriedades da Classe
+        public Guid Id { get; set; }
+        [Required(ErrorMessage = "O campo {0} é obrigatorio")]
+        [MaxLength(50, ErrorMessage = "O campo {0} deve ter no maximo {1} caracteres")]
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public int Idade { get; set; }
+    }
+}
